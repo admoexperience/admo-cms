38172b57d28a59e66acc909d79bbdf269d8c86cf
David Rubin <david@fireid.com>
2013-05-10 10:42:46 +0200

    New clean rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdmoCms::Application.config.secret_token = 'db0919a77abad38139ee241afa001d143be8391f9529541498c57aeb76c44a97678b3519be9a2dbf856deb66461be5023133158fed76f3d3b8522a752bc5bac9'
