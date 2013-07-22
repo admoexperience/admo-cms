032b01a63a6b63cef92aefba5db6fec8e1bb26d2
David Rubin <david@fireid.com>
2013-07-22 08:44:52 +0200

    Using base image name as a base folder name

--- lib/dropbox_uploader.rb
+++ lib/dropbox_uploader.rb
@@ -11,10 +11,11 @@ class DropboxUploader
   def upload_file(path,file,name)
     date = Time.now.strftime("%Y-%m-%d")
     ext =File.extname(name)
+    base_name = File.basename(name, ext)
     #Strip any thing that isn't lower case chars/numbers
     new_name = name.downcase.gsub(/[^0-9a-z\.]/, '')
     file_name = Time.now.strftime("%H-%M-%S_#{new_name}")
-    response = @client.put_file(File.join(path,date,file_name), open(file))
+    response = @client.put_file(File.join(path,base_name,date,file_name), open(file))
     Rails.logger.info response.inspect
   end
 end
