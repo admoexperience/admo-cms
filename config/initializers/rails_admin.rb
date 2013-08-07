96bb7ef6646ecae3f389110a77b0eb4ace248e7c
David Rubin <david@fireid.com>
2013-08-07 10:53:03 +0200

    Adding user info to admin console

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -68,7 +68,7 @@ end
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 
   # RailsAdmin may need a way to know who the current user is]
-  config.current_user_method {  } # auto-generated
+  config.current_user_method { current_user } # auto-generated
 
   # If you want to track changes on your models:
   # config.audit_with :history, 'User'
