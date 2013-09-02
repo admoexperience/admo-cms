87351272aa3f004c246d831ecbd21191548c0b79
David Rubin <david@fireid.com>
2013-09-02 22:20:07 +0200

    Demo seeds and config cleanup

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,11 +1,11 @@
  if Settings.email && Settings.email.password
   Rails.logger.info "Using #{Settings.email.user_name}"
-  ActionMailer::Base.default_url_options = { :protocol => 'https', :host => Settings.general.hostname }
+  ActionMailer::Base.default_url_options = { :protocol => Settings.general.hostname, :host => Settings.general.hostname }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.perform_deliveries = true
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,  
+    :enable_starttls_auto => true,
     :address            => Settings.email.smtp_hostname,
     :port               => Settings.email.smtp_port,
     :domain             => Settings.email.smtp_domain, #you can also use google.com
