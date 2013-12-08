312f06fddd254ee69d9ab91f2d163199322ce578
David Rubin <david@fireid.com>
2013-12-08 16:37:56 +0200

    Moving many secretes out of code and revoking their original values

--- app/jobs/hipchat_notify_job.rb
+++ app/jobs/hipchat_notify_job.rb
@@ -1,7 +1,11 @@
 class HipchatNotifyJob < BaseJob
 
   def perform(message, format='text')
-     client = HipChat::Client.new(Settings.hipchat.api_key)
-     result = client[Settings.hipchat.room].send('AdmoCMS', message, message_format: format)
+    unless Settings.hipchat.api_key.empty?
+       client = HipChat::Client.new(Settings.hipchat.api_key)
+       result = client[Settings.hipchat.room].send('AdmoCMS', message, message_format: format)
+    else
+      log_debug("HIPCHAT: api_key not set not sending [#{message}]")
+    end
   end
 end
--- app/jobs/pubnub_push_job.rb
+++ app/jobs/pubnub_push_job.rb
@@ -1,18 +1,22 @@
 class PubnubPushJob < BaseJob
 
   def perform(chanel_token, message)
-    pubnub = Pubnub.new(
-      :publish_key=> Settings.pubnub.publish_key,
-      :subscribe_key=> Settings.pubnub.subscribe_key,
-      :secret_key    => nil,    # optional, if used, message signing is enabled
-      :cipher_key    => nil,    # optional, if used, encryption is enabled
-      :ssl           => false     # true or default is false
-    )
+    unless Settings.pubnub.publish_key.empty? or Settings.pubnub.subscribe_key.empty?
+      pubnub = Pubnub.new(
+        :publish_key=> Settings.pubnub.publish_key,
+        :subscribe_key=> Settings.pubnub.subscribe_key,
+        :secret_key    => nil,    # optional, if used, message signing is enabled
+        :cipher_key    => nil,    # optional, if used, encryption is enabled
+        :ssl           => false     # true or default is false
+      )
 
-    pubnub.publish({
-      :channel => chanel_token,
-      :message => message,
-      :callback => lambda { |message| }
-    })
+      pubnub.publish({
+        :channel => chanel_token,
+        :message => message,
+        :callback => lambda { |message| }
+      })
+    else
+      log_debug("PUBNUB: publish/subscribe keys not set not pushing [#{message}] down [#{chanel_token}]")
+    end
   end
 end
