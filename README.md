a56acf7b9dc8d8f23003c801bf339cea5717a021
David Rubin <david@fireid.com>
2013-12-08 21:26:14 +0200

    Cleaning up

--- README.md
+++ README.md
@@ -1,27 +1,55 @@
 Getting started
 ---------------
-install [rbenv](https://github.com/sstephenson/rbenv) and [ruby-build](https://github.com/sstephenson/ruby-build)
+
+### Install Mongodb + imagemagick ###
+
+On OSX:
+
+    brew update
+    brew install mongodb
+    brew install imagemagick
+
+Install [rbenv](https://github.com/sstephenson/rbenv) and [ruby-build](https://github.com/sstephenson/ruby-build)
 
 
-Seed the Database 
-  
-  rake db:seed #NB ONLY run this in dev mode
+# Configuration #
+
+The following is provided by the `rails_config` gem.
+
+Config entries are compiled from:
+
+    config/settings.yml
+    config/settings/#{environment}.yml
+
+    config/settings.local.yml
+    config/settings/#{environment}.local.yml
+
+Settings defined in files that are lower in the list override settings higher. All `*.local.yml` files can be created per
+developer because they are `.gitignored`. All settings can be accessed through the globally defined `Settings` object.
+
+Default settings are defined in `config/settings.yml`.
+
+# Testing
+
+To run all tests
+
+    rspec
+
+
+## Development setup ##
+
+    rake db:seed
 
 defaults:
   
   * Username: demo@admoexperience.com
+  * AdminUser: admin@admoexperience.com
   * Password: demo12345
 
-To run server in dev mode
+
+## Live Reloading ##
 
     foreman start -f Procfile.dev
 
-This runs
+This runs `rails s -p 3000` and `guard`
 
-    rails s -p 3002
-
-As well as live-reload
-
-    guard
-
-Or you can run both commands separately.
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,33 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 unless Admin.first
-  Admin.create!(email: 'david@fireid.com', password: '1234', company_name: 'admincorp')
+  Admin.create!(email: 'admin@admoexperience.com', password: 'demo12345')
 end
 
 account = AdmoAccount.first
 unless account
   account = AdmoAccount.create!(name: 'Demo')
 end
-
-App.destroy_all
-
-app = App.first
-unless app
-  config = {
-      top_left:{title: 'Avatar', description:'A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.'},
-      top_right:{title: 'G.I Joe Retaliation', description:'The G.I. Joes are not only fighting their mortal enemy Cobra; they are forced to contend with threats from within the government that jeopardize their very existence.'},
-      bottom_left:{title: 'Quartet', description: "At a home for retired musicians, the annual concert to celebrate Verdi's birthday is disrupted by the arrival of Jean, an eternal diva and the former wife of one of the residents."},
-      bottom_right:{title: 'The Impossible', description:"The story of a tourist family in Thailand caught in the destruction and chaotic aftermath of the 2004 Indian Ocean tsunami."},
-  }
-  app = App.create!(name: 'DemoApp', admo_account: account, config: config)
-
-  config.each do |key, value|
-    image_file =File.new("#{Rails.root}/app/assets/images/demo-image.jpg")
-    Content.create!(app: app, value: image_file, key: "images/#{key}.jpg" )
-    Content.create!(app: app, key: "images/#{key}.webm" )
-  end
-end
-
 
 #AdmoUnit.destroy_all
 unless AdmoUnit.all.count >= 5
