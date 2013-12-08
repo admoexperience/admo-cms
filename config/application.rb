1f15792dfd9eee6be6cd99ee5d5c44400a54ab9d
David Rubin <david@fireid.com>
2013-12-08 17:59:52 +0200

    init on compile assets

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module AdmoCms
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     #Fixes journey unable to handle chunked encoding.
     config.middleware.insert_after ActionDispatch::DebugExceptions, Rack::ContentLength
